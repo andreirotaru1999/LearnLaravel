8ada9c457a413de6287d221d62ad0a5ff78fea79
andreirotaru1999 <andreirotaru1999@yahoo.com>
2020-09-30 12:00:06 +0300

    added login page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return env('APP_NAME');
     return view('welcome');
 });
+
+Route::get('/user', 'UserController@index');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
