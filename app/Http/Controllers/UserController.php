3a257a6f52d1c5d0043a78dbc50359e699232b19
andreirotaru1999 <andreirotaru1999@yahoo.com>
2020-09-30 21:38:34 +0300

    added delete old avatar image when uploading a new one

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
     public function uploadAvatar(Request $request)
     {
         if($request->hasFile('image')){
-            $filename = $request->image->getClientOriginalName();
-            $request->image->storeAs('images', $filename, 'public');
-            auth()->user()->update(['avatar'=> $filename]);
+            User::uploadAvatar($request->image);
+            
         }
         return redirect()->back();
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function uploadAvatar($image)
+    {
+        $filename = $image->getClientOriginalName();
+        (new self())->deleteOldImage();
+        $image->storeAs('images', $filename, 'public');
+        auth()->user()->update(['avatar'=> $filename]);
+        
+    }
+    protected function deleteOldImage()
+    {
+        if($this->avatar){
+            Storage::delete('/public/images/' . $this->avatar);
+        }
+    }
 }
